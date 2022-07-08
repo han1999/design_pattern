7ed90566db8d5873644ce9edb375bb92dd843c97
Han Xiao <1043599451@qq.com>
2022-07-08 21:31:12 +0800

    start design_pattern

---
+++ src/main/java/com/hanxiao/pipeline/MyContext.java
@@ -0,0 +1,13 @@
+package com.hanxiao.pipeline;
+
+import lombok.Data;
+
+/**
+ * @description:
+ * @author: Han Xiao
+ * @date: 2022/7/8
+ **/
+@Data
+public class MyContext {
+    private String name;
+}
